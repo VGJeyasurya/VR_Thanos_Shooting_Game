20ab4dd355546c24b2d2f10f1d5e5331377f3967
Narendra Kumar <30648034+nar3n12@users.noreply.github.com>
2019-09-14 18:54:07 +0530

    initial commit

---
+++ Assets/thanosZombie.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thanosZombie : MonoBehaviour {
+  
+
+  // Use this for initialization
+  void Start () {
+      //start the walking animation
+   // GetComponent<Animation>().Play ("walk");
+  }
+
+
+  //for this to work both need colliders, one must have rigid body, and the zombie must have is trigger checked.
+  void OnTriggerEnter (Collider col)
+  {
+    //first disable the zombie's collider so multiple collisions cannot occur
+    GetComponent<BoxCollider>().enabled = false;
+    //destroy the bullet
+    Destroy(col.gameObject);
+    //stop the zombie from moving forward by setting its destination to it's current position
+   // agent.destination = gameObject.transform.position;
+    //stop the walking animation and play the falling back animation
+    GetComponent<Animation>().Stop ();
+   // GetComponent<Animation>().Play ("back_fall");
+    //destroy this zombie in six seconds.
+    Destroy (gameObject, 3);
+  
+  }
+
+}
